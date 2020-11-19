1aa1584f20b0e44c25ab03f5f71940d414ad7d58
Lennart Oser <69626866+LMO94@users.noreply.github.com>
2020-11-19 11:29:51 +0000

    Update of scraper

--- company_details.py
+++ company_details.py
@@ -4,14 +4,21 @@ import pandas as pd
 import numpy as np
 import re
 
-columns = ["name", "year_founded", "team_size", "location", "batch", "short_description", "long_description"]
+columns = ["name",
+           "year_founded",
+           "team_size",
+           "location",
+           "batch",
+           "short_description",
+           "long_description"
+           ]
+
 ycombinator = pd.DataFrame(columns=columns)
 
 links = pd.read_csv("merged_working_links.csv")
-links.columns = ["index_1","index_2", "links"]
+links.columns = ["index_1", "index_2", "links"]
 links_list = links["links"].values.tolist()
 print(links_list)
-
 
 for i in np.arange(0, len(links_list)):
     url = links_list[i]
@@ -22,14 +29,11 @@ for i in np.arange(0, len(links_list)):
     name = (soup.find("div", {"h1", "heavy"})).get_text(strip=True)
     short_description = (soup.find("h3").get_text(strip=True))
     long_description = (soup.find("p").get_text(strip=True))
-
     details = (soup.find(("div"), ("highlight-box"))).get_text(strip=True)
-    matchObj = re.match(r'(.*)Founded(\d*)Team Size(\d*)Location(.*)', details,re.M|re.I)
-
+    matchObj = re.match(r'(.*)Founded(\d*)Team Size(\d*)Location(.*)', details, re.M | re.I)
     year = matchObj.group(2)
     team_size = matchObj.group(3)
     location = matchObj.group(4)
-
     batch = (soup.find(("span"), ("pill pill-orange"))).get_text(strip=True)
 
     dict = {
